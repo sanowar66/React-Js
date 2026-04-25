1e5c86699a47a5b37997f41f2b6339f6f52e67d7
Sanowar <sanowarhossain6697@gmail.com>
2026-04-25 23:52:31 +0600

    counter app

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+ 
+const [counter,setCounter]=useState(0);
+  const increment = () => {
+    setCounter(counter + 1);
+  }
+  const decrement = () => {
+    if(counter>=1)
+    setCounter(counter - 1);
+  }
+
+  return (
+    <div className="container">
+      <div className="box">
+        <h2>Counter</h2>
+
+        <div className="counter">
+          <button onClick={decrement}>-1</button>
+          <span className="quantity">{counter}</span>
+          <button onClick={increment}>+1</button>
+        </div>
+        <p>Total Order: {counter}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
