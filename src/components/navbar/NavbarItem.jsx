9ab5570be4df82812d65449fdd3ab1172afdaded
Sanowar <sanowarhossain6697@gmail.com>
2026-04-29 21:37:43 +0600

    practice to how to export import smartly sothat decrease code

--- src/components/navbar/NavbarItem.jsx
+++ src/components/navbar/NavbarItem.jsx
@@ -2,7 +2,7 @@ export const NavbarItem = () => {
     return (
         <div className="para"> 
             <div>
-                <p>Just practice  how to build folder and connect</p>
+                <p>Just practice  how to build folder and how to import and export other folder </p>
             </div>
         </div>   
     );
