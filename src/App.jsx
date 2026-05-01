ec1e8e72eb5be8b0933ec50983ac3cae7dc12f81
Sanowar <sanowarhossain6697@gmail.com>
2026-05-01 22:01:01 +0600

    login & logout authenticaded in css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import "./App.css";
+
+
 import{ Footer} from "./components/footer";
 import { Navbar,NavbarItem } from "./components/navbar";
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,20 @@
 import { NavbarItem } from "./NavbarItem";
-
+import './Navbar.css'
   export const Navbar = () => {
+    const isAuthenticated=true;
   return (
     <div class="nave-bar">
       <div>
         <p>Logo Here</p>
       </div>
       <div>
-        <ul>
-          <NavbarItem title="Login"/>
+        <ul className="menu-item">
           <NavbarItem title="Contact"/>
           <NavbarItem title="Porduct"/>
           <NavbarItem title="Service"/>
           <NavbarItem title="Community"/>
+         {isAuthenticated ? <NavbarItem title="Logout" />  : <NavbarItem title="Login" />
+  }
         </ul>
       </div>
     </div>
